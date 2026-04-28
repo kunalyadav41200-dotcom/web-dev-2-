88fdee30565d268b0425ab35147fa68de77b8805
Kunal Yadav <kunalyadav41200@gmail.com>
2026-04-28 15:48:19 +0530

    file

--- reactapp/src/App.jsx
+++ reactapp/src/App.jsx
@@ -7,19 +7,19 @@ import Contact from './component/Contact'
 import { Route, Routes } from 'react-router-dom'
 
 function App() {
-  const[data,setData]=useState("Sample data")
+  const [data, setData] = useState("Sample data")
   return (
     <>
+      <UserContext.Provider value={name}>
+        <Routes>
+          <Route path="/article" element={<Article data={data} />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/" element={<><Homepage /><Menu /></>} />
+          <Route path="/profile/:username" element={<Profile />} />
+        </Routes>
 
-      <Routes>
-        <Route path="/article" element={<Article data={data} />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/" element={<><Homepage /><Menu/></>} />
-        <Route path="/profile/:username" element={<Profile />} />
-      </Routes>
-      
-    </>
-  )
+      </>
+      )
 }
 
-export default App
+      export default App
